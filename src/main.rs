390e72a7d9c3317c55778de4ff41830abf436484
Hamster3600 <dawidch2018@gmail.com>
2026-06-15 22:16:53 +0200

    change of language from polish to english and adding forwarding messages

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,138 @@
 use std::thread;
 use std::io::{self, Read, Write};
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tiny_http::{Server, Response};
 use keyring_core::Entry;
 use uuid::Uuid;
 use serde::{Serialize, Deserialize};
 
-// STRUKTURY JSON
 #[derive(Serialize, Deserialize, Debug)]
-struct RejestracjaPaczka {
+struct RegistrationPacket {
     nick: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct WiadomoscPaczka {
-    cel_hash: String,
-    nadawca_hash: String,
-    nadawca_nick: String,
-    tresc: String,
+struct MessagePacket {
+    target_hash: String,
+    sender_hash: String,
+    sender_nick: String,
+    content: String,
+}
+
+type RoutingTable = Arc<Mutex<HashMap<String, String>>>;
+
+fn forward_message(packet_body: &str, client_ip: String, routing_table: &RoutingTable, http_client: &reqwest::blocking::Client,) -> Result<String, String>{
+    let message: MessagePacket = serde_json::from_str(packet_body).map_err(|e| format!("Failed to parse message packet: {}", e))?;
+
+    let client_address = format!("{}:4001", client_ip);
+
+    {
+        let mut table = routing_table.lock().unwrap();
+        table.insert(message.sender_hash.clone(), client_address);
+    }
+
+    let table = routing_table.lock().unwrap();
+    if let Some(target_address) = table.get(&message.target_hash){
+        let url = format!("http://{}", target_address);
+        match http_client.post(&url).body(packet_body.to_string()).send(){
+            Ok(_) => Ok(format!("Message rwarded to {} with hash {}", target_address, message.target_hash)),
+            Err(e) => Err(format!("Target is registered, but connection to port 4001 failed: {}", e)),
+        }
+    }   else{
+        Err(format!("Target hash {} is offline or not existing(not in routing table)", message.target_hash))
+    }
 }
 
 fn main() {
     let config = HashMap::new();
     keyring::use_sqlite_store(&config).unwrap();
 
-    println!("[COMUNICATOR-SERVER] Uruchamiam węzeł serwera na wątkach...");
+    let routing_table: RoutingTable = Arc::new(Mutex::new(HashMap::new()));
+    let http_client = reqwest::blocking::Client::new();
+
+    println!("[COMUNICATOR-SERVER] Running a server node on threads...");
 
     let server = Server::http("0.0.0.0:4001").unwrap();
-    println!("[COMUNICATOR-SERVER] Serwer gotowy na porcie 4001...");
+    println!("[COMUNICATOR-SERVER] Server is ready on port 4001...");
     println!("--------------------------------------------------");
 
     for mut request in server.incoming_requests() {
+        let routing_table = Arc::clone(&routing_table);
+        let http_client = http_client.clone();
         thread::spawn(move || {
             let mut body = String::new();
             
             if request.as_reader().read_to_string(&mut body).is_ok() {
                 
-                if let Ok(paczka_rej) = serde_json::from_str::<RejestracjaPaczka>(&body) {
-                    let nick = &paczka_rej.nick;
+                if let Ok(packet_req) = serde_json::from_str::<RegistrationPacket>(&body) {
+                    let nick = &packet_req.nick;
 
-                    let wpis_sprawdzenia_nicku = Entry::new("Comunicator-server", &format!("{}_hash", nick)).unwrap();
-                    if wpis_sprawdzenia_nicku.get_secret().is_ok() {
-                        println!("[REJESTRACJA] Odmowa. Nick \"{}\" jest już zajęty.", nick);
-                        let response = Response::from_string("BŁĄD: Nick jest już zajęty.");
+                    let check_entry_nick = Entry::new("Comunicator-server", &format!("{}_hash", nick)).unwrap();
+                    if check_entry_nick.get_secret().is_ok() {
+                        println!("[REJESTRACJA] Refusal. Nick \"{}\" is already taken.", nick);
+                        let response = Response::from_string("[ERROR] Nick is already taken.");
                         let _ = request.respond(response);
                         return;
                     }
                     
-                    let mut nowy_hash = String::new();
+                    let mut new_hash = String::new();
                     loop {
-                        let potencjalny_hash = Uuid::new_v4().to_string();
-                        let czy_unikalne = Entry::new("Comunicator-server", &potencjalny_hash).unwrap().get_secret().is_err();
-                        if czy_unikalne {
-                            nowy_hash = potencjalny_hash;
+                        let potential_hash = Uuid::new_v4().to_string();
+                        let is_unique = Entry::new("Comunicator-server", &potential_hash).unwrap().get_secret().is_err();
+                        if is_unique {
+                            new_hash = potential_hash;
                             break;
                         }
                     }
                     
-                    println!("\n[NOWY UŻYTKOWNIK] Rejestracja nicku: \"{}\"", nick);
-                    println!("  -> Wygenerowano stały Hash: {}", nowy_hash);
+                    println!("\n[NEW USER] Registration on nick: \"{}\"", nick);
+                    println!("  -> Generated permanent Hash: {}", new_hash);
                     println!("--------------------------------------------------");
 
-                    let wpis_hash = Entry::new("Comunicator-server", &format!("{}_hash", nick)).unwrap();
-                    let wpis_nick = Entry::new("Comunicator-server", &format!("{}_nick", nick)).unwrap();
-                    let wpis_odwrotny = Entry::new("Comunicator-server", &nowy_hash).unwrap();
+                    let entry_hash = Entry::new("Comunicator-server", &format!("{}_hash", nick)).unwrap();
+                    let entry_nick = Entry::new("Comunicator-server", &format!("{}_nick", nick)).unwrap();
+                    let entry_reverse = Entry::new("Comunicator-server", &new_hash).unwrap();
                     
-                    wpis_odwrotny.set_secret(nick.as_bytes()).unwrap();
-                    wpis_hash.set_secret(nowy_hash.as_bytes()).unwrap();
-                    wpis_nick.set_secret(nick.as_bytes()).unwrap();
+                    entry_reverse.set_secret(nick.as_bytes()).unwrap();
+                    entry_hash.set_secret(new_hash.as_bytes()).unwrap();
+                    entry_nick.set_secret(nick.as_bytes()).unwrap();
 
-                    let response = Response::from_string(nowy_hash);
+                    let response = Response::from_string(new_hash);
                     let _ = request.respond(response);
                     return;
                 } 
                 
-                if let Ok(paczka_msg) = serde_json::from_str::<WiadomoscPaczka>(&body) {
+                if let Ok(packet_msg) = serde_json::from_str::<MessagePacket>(&body) {
                     println!("\n[PACZKA DANYCH JSON]");
-                    println!("  Od (Hash):   {}", paczka_msg.nadawca_hash);
-                    println!("  Nick:        {}", paczka_msg.nadawca_nick);
-                    println!("  Do (Hash):   {}", paczka_msg.cel_hash);
-                    println!("  Wiadomość:   {}", paczka_msg.tresc);
+                    println!("  From (Hash):   {}", packet_msg.sender_hash);
+                    println!("  Nick:          {}", packet_msg.sender_nick);
+                    println!("  To (Hash):     {}", packet_msg.target_hash);
+                    println!("  Message:       {}", packet_msg.content);
                     println!("--------------------------------------------------");
-                    
-                    let response = Response::from_string("Serwer: Wiadomość przetworzona.");
-                    let _ = request.respond(response);
+
+                    let sender_ip = request.remote_addr().unwrap().ip().to_string();
+
+                    if body.contains("target_hash") {
+                        match forward_message(&body, sender_ip, &routing_table, &http_client) {
+                            Ok(succes_msg) => {
+                                println!("[SERVER] {}", succes_msg);
+                                let response = tiny_http::Response::from_string("[SERVER] Message forwarded successfully.");
+                                let _ = request.respond(response);
+                            }
+                            Err(error_msg) => {
+                                println!("[ERROR] {}", error_msg);
+                                let response = tiny_http::Response::from_string(format!("[SERVER] Error forwarding message: {}", error_msg));
+                                let _ = request.respond(response);
+                            }
+                        }
+                    }
                     return;
                 }
 
-                let response = Response::from_string("Serwer: Błędny format danych (Oczekiwano JSON).");
+                let response = Response::from_string("[SERVER] Invalid data format (Expected JSON).");
                 let _ = request.respond(response);
             }
         });
+        }
     }
-}
